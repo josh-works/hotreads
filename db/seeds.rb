5b7c87e421b6abb216cfd0a6b9013014aac39f12
Josh Thompson <thompsonjoshd@gmail.com>
2017-08-02 01:54:34 -0600

    seeds, basic rendering on root

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Link.destroy_all
+
+Link.create!(url: "http://jkglei.com/progress/")
+Link.create!(url: "http://localhost:3000/")
+Link.create!(url: "https://gist.github.com/neight-allen/4cb05991a97ac9970dc1620d1f1734fb")
+Link.create!(url: "https://vimeo.com/198611383")
+Link.create!(url: "https://www.google.com/search?q=rails+new+postgress+rspec&oq=rails+new+")
+Link.create!(url: "http://julianveling.com/?p=18")
